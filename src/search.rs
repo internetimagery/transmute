6b75972c300b47f6152c1fa5940f60cf0e3ccb4e
Jason Dixon <jason.dixon.email@gmail.com>
2020-05-03 14:33:02 +1200

    more progress getting basic search working

--- src/search.rs
+++ src/search.rs
@@ -1,31 +1,58 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::sync::Arc;
+use std::rc::Rc;
 
 // python int
 pub type Int = isize;
 
 // Representing an edge between two nodes
 // transforming from one thing to another
-#[derive(Hash, Eq, PartialEq, Debug)]
+#[derive(Hash, Eq, PartialEq, Debug, Ord, PartialOrd)]
 pub struct Edge {
     cost: Int,
     hash_in: Int,
     hash_out: Int,
 }
 
+#[derive(Hash, Eq, PartialEq, Debug, Ord, PartialOrd)]
+pub struct State<'a> {
+    cost: Int,
+    actual_cost: Int,
+    edge: &'a Edge,
+    parent: Option<Rc<State<'a>>>,
+}
+
 // Our graph!
 pub struct Graph {
     // TODO this needs to be a vector/set of Edges
-    map_in: HashMap<Int, HashSet<Arc<Edge>>>,
-    map_out: HashMap<Int, HashSet<Arc<Edge>>>,
+    edges_in: HashMap<Int, HashSet<Arc<Edge>>>,
+    edges_out: HashMap<Int, HashSet<Arc<Edge>>>,
+}
+
+
+impl<'a> State<'a> {
+    fn new(cost: Int, edge: &'a Edge, parent: Option<Rc<State<'a>>>) -> Self {
+        let actual_cost = edge.cost
+            + match &parent {
+                Some(p) => p.cost,
+                None => 0,
+            };
+        Self {
+            cost,
+            actual_cost,
+            edge,
+            parent,
+        }
+    }
 }
 
 impl Graph {
     // Create a new graph
     pub fn new() -> Self {
         Graph {
-            map_in: HashMap::new(),
-            map_out: HashMap::new(),
+            edges_in: HashMap::new(),
+            edges_out: HashMap::new(),
         }
     }
 
@@ -36,15 +63,98 @@ impl Graph {
             hash_in,
             hash_out,
         });
-        let edges_in = self.map_in.entry(hash_in).or_insert(HashSet::new());
-        let edges_out = self.map_out.entry(hash_in).or_insert(HashSet::new());
+        let edges_in = self.edges_in.entry(hash_in).or_insert(HashSet::new());
+        let edges_out = self.edges_out.entry(hash_out).or_insert(HashSet::new());
         edges_in.insert(Arc::clone(&edge));
         edges_out.insert(Arc::clone(&edge));
     }
 
     // Search the graph to find what we want to find
-    pub fn search(&self, hash_in: Int, hash_out: Int) {
-        // TODO port across search logic
-        println!("SEARCHING!");
+    pub fn search(&self, hash_in: Int, hash_out: Int) -> Option<()> {
+        // Get our starting points!
+        let mut queue_in: BinaryHeap<_> = match self.edges_in.get(&hash_in) {
+            Some(edges) => edges.iter().map(|e| Reverse(State::new(e.cost, e, None))).collect(),
+            None => BinaryHeap::new(),
+        };
+        let mut queue_out: BinaryHeap<_> = match self.edges_out.get(&hash_out) {
+            Some(edges) => edges.iter().map(|e| Reverse(State::new(e.cost, e, None))).collect(),
+            None => BinaryHeap::new(),
+        };
+
+
+        // Track where we have been
+        let mut visited_in = HashSet::new();
+        let mut visited_out = HashSet::new();
+
+        // Search our graph!
+        loop {
+            if !queue_in.is_empty() && queue_in.len() < queue_out.len() {
+                // //////////////// //
+                // Search forwards! //
+                // //////////////// //
+                let state = match queue_in.pop() {
+                    Some(Reverse(s)) =>  s,
+                    _ => continue
+                };
+
+                // Check if we have reached our goal
+                if state.edge.hash_out == hash_out {
+                    println!("We did it!");
+                    return Some(());
+                }
+
+                // Mark where we have been
+                visited_in.insert(state.edge);
+
+                // Search further into the graph!
+                if let Some(edges) = self.edges_in.get(&state.edge.hash_out) {
+                    let state_rc = Rc::new(state);
+                    for edge in edges {
+                        if visited_in.contains(edge.as_ref()) {
+                            continue
+                        }
+                        queue_in.push(
+                            Reverse(State::new(1, edge, Some(Rc::clone(&state_rc))))
+                        )
+                    }
+                }
+
+            } else if !queue_out.is_empty() {
+                // ///////////////// //
+                // Search backwards! //
+                // ///////////////// //
+                let state = match queue_out.pop() {
+                    Some(Reverse(s)) =>  s,
+                    _ => continue
+                };
+
+                // Check if we have reached our goal
+                if state.edge.hash_in == hash_in {
+                    println!("We did it!");
+                    return Some(());
+                }
+
+                // Mark where we have been
+                visited_out.insert(state.edge);
+
+                // Search further into the graph!
+                if let Some(edges) = self.edges_out.get(&state.edge.hash_in) {
+                    let state_rc = Rc::new(state);
+                    for edge in edges {
+                        if visited_in.contains(edge.as_ref()) {
+                            continue
+                        }
+                        queue_out.push(
+                            Reverse(State::new(1, edge, Some(Rc::clone(&state_rc))))
+                        )
+                    }
+                }
+
+            } else {
+                break;
+            }
+        }
+        println!("Queue is empty");
+        None
     }
 }
--- test.py
+++ test.py
@@ -1,10 +1,60 @@
-import sys
 
+import sys, itertools
 sys.path.append("target/release")
+from transmute import Grimoire
 
-import transmute
+module = sys.modules[__name__]
 
-grimoire = transmute.Grimoire()
-grimoire.inscribe_transmutation(1, str, int)
-grimoire.transmute("123", int)
-print(grimoire)
+class Executor(object):
+    def __init__(self, variation=""):
+        self.variation = variation and ":" + variation
+
+    def __call__(self, value):
+        return value + " -> " + self.__class__.__name__ + self.variation
+
+letters = "ABCDEFG"
+
+for letter in letters:
+    setattr(module, "TYPE_" + letter, type("TYPE_" + letter, (Executor,), {}))
+
+for a, b in itertools.permutations(letters, 2):
+    setattr(module, a + "to" + b, type(a + "to" + b, (Executor,), {}))
+
+def bad_transmuter(_):
+    raise RuntimeError("BAD STUFF")
+
+# A - B - C - D
+# |         /
+# E - F - G
+
+grimoire = Grimoire()
+grimoire.inscribe_transmutation(1, TYPE_A, TYPE_B, AtoB())
+grimoire.inscribe_transmutation(1, TYPE_A, TYPE_E, AtoE())
+grimoire.inscribe_transmutation(1, TYPE_B, TYPE_C, BtoC())
+grimoire.inscribe_transmutation(1, TYPE_C, TYPE_D, CtoD())
+grimoire.inscribe_transmutation(1, TYPE_E, TYPE_F, EtoF())
+grimoire.inscribe_transmutation(1, TYPE_F, TYPE_G, FtoG())
+grimoire.inscribe_transmutation(1, TYPE_G, TYPE_D, GtoD())
+
+assert (
+    grimoire.transmute("start", TYPE_D, TYPE_A)
+    == "start -> AtoB -> BtoC -> CtoD"
+)
+
+# A           E
+#  \         /
+#   - C - D -
+#  /         \
+# B           F
+
+grimoire = Grimoire()
+grimoire.inscribe_transmutation(1, TYPE_A, TYPE_C, AtoC())
+grimoire.inscribe_transmutation(1, TYPE_B, TYPE_C, BtoC())
+grimoire.inscribe_transmutation(1, TYPE_C, TYPE_D, CtoD())
+grimoire.inscribe_transmutation(1, TYPE_D, TYPE_E, DtoE())
+grimoire.inscribe_transmutation(1, TYPE_D, TYPE_F, DtoF())
+
+assert (
+    grimoire.transmute("start", TYPE_F, TYPE_A)
+    == "start -> AtoC -> CtoD -> DtoF"
+)
